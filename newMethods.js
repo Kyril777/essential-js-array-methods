b260f51008a1f0872207eeb70e1ede75358f8203
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-08-05 23:23:00 -0400

    Update newMethods.js

--- newMethods.js
+++ newMethods.js
@@ -3,3 +3,12 @@ const flowers = ['lily', 'tulip', 'jasmine', 'tulip'];
 const uniqueFlowers = [...new Set(flowers)];
 
 console.log(uniqueFlowers); // Result: ["lily", "tulip", "jasmine"]
+
+
+// Find out the minimum and maximum values of an array.
+const values = [156725, 191437, 111019, 187721, 176003, 124958];
+let minimum = Math.min(...values);
+let maximum = Math.max(...values);
+
+console.log(minimum); // Output: 111019
+console.log(maximum); // Output: 191437
